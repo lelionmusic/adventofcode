96951976ba3e4a78d29d353c09bc04d517bd9b6b
Geir Olav Alsvik <goagoa95@gmail.com>
2019-12-03 14:46:54 +0100

    changed

--- 2019/1-1.py
+++ 2019/1-1.py
@@ -2,13 +2,14 @@ def calculate_fuel(mass):
     return int(mass) // 3 - 2
 
 total_1 = total_2 = 0
-with open("input/input1.txt") as data:
-    for mass in data:
-        fuel = calculate_fuel(mass)
-        total_1 += fuel
-        while fuel > 0:
-            total_2 += fuel
-            fuel = calculate_fuel(fuel)
+lines = open("input/input1.txt").readlines()
+
+for mass in lines:
+    fuel = calculate_fuel(mass)
+    total_1 += fuel
+    while fuel > 0:
+        total_2 += fuel
+        fuel = calculate_fuel(fuel)
 
 print("Part 1:", total_1)
 print("Part 2:", total_2)
@@ -20,3 +21,6 @@ def calculate_total_fuel_recursively(mass):
         return recurse(fuel + extra, calculate_fuel(extra))
     fuel = calculate_fuel(mass)
     return recurse(fuel, calculate_fuel(fuel))
+
+# Pythonic part 1
+sum([int(mass) // 3 - 2 for mass in lines])
