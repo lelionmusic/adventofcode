6d6f1b92c7be4d210c96c6a2809d259645b42f1a
Geir Olav Alsvik <goa95@protonmail.com>
2020-12-05 14:50:03 +0100

    cleanup

--- 2020/05.py
+++ 2020/05.py
@@ -1,23 +1,17 @@
-class Seat:
-    def __init__(self, pos):
-        row_min, row_max = 0, 127
-        col_min, col_max = 0, 7
-        for c in pos:
-            if   c == 'F': row_max -= (row_max - row_min) // 2 + 1
-            elif c == 'B': row_min += (row_max - row_min) // 2 + 1
-            elif c == 'L': col_max -= (col_max - col_min) // 2 + 1
-            elif c == 'R': col_min += (col_max - col_min) // 2 + 1
-        self.row = row_min
-        self.col = col_min
-        self.id = row_min * 8 + col_min
+def find_id(pos):
+    row_min, row_max = 0, 127
+    col_min, col_max = 0, 7
+    for c in pos:
+        if   c == 'F': row_max -= (row_max - row_min) // 2 + 1
+        elif c == 'B': row_min += (row_max - row_min) // 2 + 1
+        elif c == 'L': col_max -= (col_max - col_min) // 2 + 1
+        elif c == 'R': col_min += (col_max - col_min) // 2 + 1
+    return row_min * 8 + col_min
 
-    def __lt__(self, other):
-        return self.id < other.id
+seat_ids = sorted([find_id(line) for line in open('05-input.txt').readlines()])
+print("Part 1:", max(seat_ids))
 
-seats = sorted([Seat(line) for line in open('05-input.txt').readlines()])
-print("Part 1:", max([seat.id for seat in seats]))
-
-for i in range(len(seats) - 1):
-    if seats[i + 1].id == seats[i].id + 2: 
-        print("Part 2:", seats[i].id + 1)
+for i in range(len(seat_ids) - 1):
+    if seat_ids[i + 1] == seat_ids[i] + 2: 
+        print("Part 2:", seat_ids[i] + 1)
         break
