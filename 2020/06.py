7d811b45c81823fe550d7a1189be236bdec71f0a
Geir Olav Alsvik <goa95@protonmail.com>
2020-12-06 14:49:23 +0100

    d6 p1 oneliner

--- 2020/06.py
+++ 2020/06.py
@@ -1,10 +1,13 @@
 def sum_group_part1(group):
-    print(group)
-    return len(set(''.join(group)))
+    # Ex: Turns ['ab', 'ac'] into ['abac'] and then into {'a', 'b', 'c'} and then into 3
+    return len(set(''.join(group))) 
 def sum_group_part2(group): 
+    # Ex: Turns ['ab', 'ac'] into [{a, b}, {a, c}] (as iterator), and then return length of intersection
     return len(set.intersection(*map(set, group)))
 
 with open("06-exampleinput.txt") as input:
     groups = [persons.split("\n") for persons in input.read().split("\n\n")]
     print("Part 1:", sum(map(sum_group_part1, groups)))
     print("Part 2:", sum(map(sum_group_part2, groups)))
+
+print(sum([len(set(x.replace("\n", ""))) for x in open("06-exampleinput.txt").read().split("\n\n")]))
